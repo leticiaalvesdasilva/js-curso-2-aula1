cf11369cf5dce2e805887ea42aa87be144ef89c5
Leticia <alvesleticia42@yahoo.com>
2025-01-17 13:04:53 -0300

    add aos metodos math.random , return, function

---
+++ app.js
@@ -0,0 +1,24 @@
+let numeroSecreto = gerarNumeroAleatorio();
+
+function exibirTextoNaTela(tag,texto){
+    let titulo = document.querySelector(tag);
+    titulo.innerHTML = texto;
+}
+
+exibirTextoNaTela('h1','Jogo do número secreto');
+exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
+
+function verificarChute(){
+    let chute = document.querySelector('input').value;
+    console.log(chute == numeroSecreto);
+}
+
+function gerarNumeroAleatorio (){
+   return parseInt(Math.random()*10 + 1);
+}
+
+
+
+
+
+
